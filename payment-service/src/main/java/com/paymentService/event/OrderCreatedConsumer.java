3c27760bffa0188df4a88cee39f64d04d8d0c703
HugoTosi <hugotosi42@gmail.com>
2026-03-17 11:02:24 -0300

    Injetando service

--- payment-service/src/main/java/com/paymentService/event/OrderCreatedConsumer.java
+++ payment-service/src/main/java/com/paymentService/event/OrderCreatedConsumer.java
@@ -1,14 +1,20 @@
 package com.paymentService.event;
 
 import com.paymentService.dto.OrderCreated;
+import com.paymentService.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class OrderCreatedConsumer {
+    @Autowired
+    PaymentService paymentService;
 
     @KafkaListener(topics = "order-created-topic", groupId = "payment-service")
     public void reciveOrderCreated(OrderCreated order){
-        System.out.println("Evento recebido: " + order.toString());
+        paymentService.processPayment(order);
+
     }
 }
